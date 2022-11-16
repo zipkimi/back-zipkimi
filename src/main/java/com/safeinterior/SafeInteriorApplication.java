9264e22c4e7b44df1a9753df8c94dece8a0dd83c
promiseyoo <promiseyoo@tuneit.io>
2022-11-16 14:31:27 +0900

    Create Project

---
+++ src/main/java/com/safeinterior/SafeInteriorApplication.java
@@ -0,0 +1,13 @@
+package com.safeinterior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeInteriorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeInteriorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safeinterior/SafeInteriorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safeinterior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeInteriorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
