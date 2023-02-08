1833ffb644c7d1e3787295b68f36cc72cc942f8d
promise_yoo <ypr821@gmail.com>
2023-02-08 15:26:26 +0900

    Docs : "사기예방법 게시판 DTO Swagger 적용"
    - Swagger 어노테이션으로 개발문서 내용 추가
    
    Related to: #3

--- src/main/java/com/safeinterior/board/dto/request/FraudPreventionGetRequest.java
+++ src/main/java/com/safeinterior/board/dto/request/FraudPreventionGetRequest.java
@@ -9,12 +9,15 @@ import lombok.Setter;
 @Getter
 @ApiModel
 public class FraudPreventionGetRequest {
-	@ApiModelProperty(name = "유저 ID", value = "", example = "1", required = true, notes = "")
+	@ApiModelProperty(name = "사용자 고유번호", value = "", example = "1", required = true, notes = "")
 	private Long userId;
 
+	@ApiModelProperty(name = "제목", value = "", example = "TEXT 제목", required = true, notes = "영문기준 200자 이하")
 	private String title;
 
+	@ApiModelProperty(name = "부제목", value = "", example = "TEXT 부제목", required = false, notes = "영문기준 300자 이하")
 	private String subTitle;
 
+	@ApiModelProperty(name = "내용", value = "", example = "TEXT 내용", required = true, notes = "")
 	private String content;
 }
--- src/main/java/com/safeinterior/board/dto/response/FraudPreventionGetsResponse.java
+++ src/main/java/com/safeinterior/board/dto/response/FraudPreventionGetsResponse.java
@@ -2,6 +2,8 @@ package com.safeinterior.board.dto.response;
 
 import java.time.ZonedDateTime;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,18 +13,25 @@ import lombok.Setter;
 @Setter
 @Getter
 @Builder
+@ApiModel
 @NoArgsConstructor
 @AllArgsConstructor
 public class FraudPreventionGetsResponse {
+	@ApiModelProperty(name = "게시글 고유번호", value = "", example = "1", required = true, notes = "")
 	private Long id;
 
+	@ApiModelProperty(name = "제목", value = "", example = "TEXT 제목", required = false, notes = "영문기준 200자 이하")
 	private String title;
 
+	@ApiModelProperty(name = "부제목", value = "", example = "TEXT 부제목", required = false, notes = "영문기준 300자 이하")
 	private String subTitle;
 
+	@ApiModelProperty(name = "내용", value = "", example = "TEXT 내용", required = false, notes = "")
 	private String content;
 
+	@ApiModelProperty(name = "게시글 유형", value = "", example = "fraudPrevention", required = false, notes = "")
 	private String type;
 
+	@ApiModelProperty(name = "게시글 등록일자", value = "", example = "2022-12-26 17:22:32", required = false, notes = "")
 	private ZonedDateTime regDt;
 }
