386e38a88d3a102b66f11c65a3bccc8834ceff5f
promise_yoo <ypr821@gmail.com>
2023-02-25 23:51:33 +0900

    Feat : "Exception file 추가"
    - BadRequestException.java 추가 (세부 내용 추가 필요)
    
    Related to: #3

--- src/main/java/com/safeinterior/exception/BadRequestException.java
+++ src/main/java/com/safeinterior/exception/BadRequestException.java
@@ -1,4 +1,10 @@
 package com.safeinterior.exception;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class BadRequestException extends RuntimeException{
+	// TODO Exception 구성 공부 하고 적용하기
+	// TODO Controller Response 공부하고 적용하기
+
 }
