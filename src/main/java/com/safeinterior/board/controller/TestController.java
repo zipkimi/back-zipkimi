2dddc3b68b210c54163b1a5b11541074eac0f12c
promise_yoo <ypr821@gmail.com>
2023-03-26 20:02:30 +0900

    Test : "TestController 의 Test 코드 추가"
    - add TestControllerTest.java
    
    Related to: #14

--- src/main/java/com/safeinterior/board/controller/TestController.java
+++ src/main/java/com/safeinterior/board/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.safeinterior.board.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.safeinterior.board.dto.request.TestDto;
 
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
@@ -14,6 +17,11 @@ public class TestController {
 	@GetMapping("/health")
 	public String health() {
 		return "hello world! :)";
+	}
+
+	@GetMapping("/health/{name}")
+	public String health(@PathVariable("name") TestDto dto) {
+		return "hello "+ dto.getName() +" world! :)";
 	}
 
 }
---
+++ src/main/java/com/safeinterior/board/dto/request/TestDto.java
@@ -0,0 +1,10 @@
+package com.safeinterior.board.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TestDto {
+	String name;
+}
