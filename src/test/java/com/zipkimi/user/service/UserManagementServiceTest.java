6c9408fd346721cde6920304907a187eec17df04
promiseyoo <ypr821@gmail.com>
2023-07-25 15:37:21 +0900

    Chore : "패키지 구조 변경" - 계층형 패키지 구조에서 일부 user, builder, admin 도메인형 패키지 구조 적용 - 공통 클래스를 global 패키지 하위로 이동
    Related to: #1

---
+++ src/test/java/com/zipkimi/user/service/UserManagementServiceTest.java
@@ -0,0 +1,61 @@
+package com.zipkimi.user.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.zipkimi.entity.SmsAuthEntity;
+import com.zipkimi.entity.UserEntity;
+import com.zipkimi.repository.SmsAuthRepository;
+import com.zipkimi.repository.UserRepository;
+import com.zipkimi.user.dto.request.SmsAuthNumberPostRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceTest {
+
+    @Autowired
+    UserManagementService userManagementService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    SmsAuthRepository smsAuthRepository;
+
+    @Test
+    public void userTest() {
+        SmsAuthEntity smsAuth = new SmsAuthEntity();
+        smsAuth.setSmsAuthNumber("1234");
+        smsAuth.setPhoneNumber("01012345678");
+        smsAuth.setIsAuthenticate(false);
+        smsAuthRepository.save(smsAuth);
+
+        UserEntity user = new UserEntity();
+        user.setName("유푸름");
+        user.setEmail("ypr821@gmail.com");
+        user.setPassword("test123");
+        user.setPhoneNumber("01012345678");
+        user.setBuilderId("general");
+        userRepository.save(user);
+    }
+
+    @Test
+    void sendSmsAuthNumberSuccessTest() {
+        // given
+        SmsAuthNumberPostRequest request = SmsAuthNumberPostRequest.builder()
+                .phoneNumber("").build();
+        // then                               // when
+        assertEquals("인증번호 전송 완료", userManagementService.sendSmsAuthNumber(request).getResult());
+    }
+
+    @Test
+    void sendSmsAuthNumberFailTest() {
+        // 이미 등록한 전화번호
+    }
+
+    @Test
+    void sendSmsAuthNumberWithoutPhoneNumberFailTest() {
+        // 요청시 입력받은 전화번호가 null 인 경우
+    }
+}
