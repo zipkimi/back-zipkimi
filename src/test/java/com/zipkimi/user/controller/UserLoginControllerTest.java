384edf39e8b91bbed48ba042ad8ea6060c8048da
Woolly <wool_ly@naver.com>
2023-08-01 15:26:47 +0900

    Test : 아이디/비밀번호 찾기 Controller 테스트 코드 추가 및 수정 - 아이디 찾기 테스트 코드 수정 - 비밀번호 찾기 테스트 코드 추가
    Related to: #17

--- src/test/java/com/zipkimi/user/controller/UserLoginControllerTest.java
+++ src/test/java/com/zipkimi/user/controller/UserLoginControllerTest.java
@@ -3,66 +3,103 @@ package com.zipkimi.user.controller;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-import com.zipkimi.user.dto.request.FindIdVerifySmsRequest;
+import com.zipkimi.entity.SmsAuthEntity;
+import com.zipkimi.entity.UserEntity;
+import com.zipkimi.repository.SmsAuthRepository;
+import com.zipkimi.repository.UserRepository;
+import com.zipkimi.user.dto.request.FindIdCheckSmsGetRequest;
+import com.zipkimi.user.dto.request.FindPwCheckSmsGetRequest;
+import com.zipkimi.user.dto.request.PassResetSmsAuthNumberPostRequest;
 import com.zipkimi.user.dto.request.SmsAuthNumberPostRequest;
-import com.zipkimi.user.dto.response.SendFindSmsAuthNumberPostResponse;
+import com.zipkimi.user.dto.response.FindSmsAuthNumberGetResponse;
+import com.zipkimi.user.dto.response.FindSmsAuthNumberPostResponse;
 import com.zipkimi.user.service.UserLoginService;
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
 class UserLoginControllerTest {
 
     @Mock
-    private UserLoginService loginService;
+    private UserLoginService userLoginService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private SmsAuthRepository smsAuthRepository;
 
     @InjectMocks
-    private UserLoginController loginController;
+    private UserLoginController userLoginController;
+
+    private static final String CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     // ************* 아이디 찾기 *************
 
     @Test
-    void sendFindIdSmsAuthNumberSuccessTest(){
+    @DisplayName(value = "아이디 찾기 - SMS 인증번호 전송 성공 테스트")
+    void sendFindIdSmsAuthNumberSuccessTest() throws Exception {
 
         //given
-        SmsAuthNumberPostRequest requestDto = new SmsAuthNumberPostRequest("01094342762");
-        SendFindSmsAuthNumberPostResponse expectedResponse = new SendFindSmsAuthNumberPostResponse("01094342762");
+        UserEntity user = new UserEntity();
+        user.setPhoneNumber("01094342762");
+        user.setEmail("test@gmail.com");
+        userRepository.save(user);
 
-        when(loginService.sendFindIdSmsAuthNumber(requestDto)).thenReturn(expectedResponse);
+        SmsAuthNumberPostRequest requestDto = new SmsAuthNumberPostRequest(user.getPhoneNumber());
+
+        when(userLoginService.sendFindIdSmsAuthNumber(requestDto)).thenReturn(
+                FindSmsAuthNumberPostResponse
+                        .builder()
+                        .result("인증번호를 전송하였습니다.")
+                        .build()
+        );
 
         //when
-        ResponseEntity<SendFindSmsAuthNumberPostResponse> responseEntity = loginController.sendFindIdSmsAuthNumber(requestDto);
+        ResponseEntity<FindSmsAuthNumberPostResponse> response = userLoginController.sendFindIdSmsAuthNumber(requestDto);
+        System.out.println("sendFindIdSmsAuthNumberFailureTest response.getBody().getResult() = " + response.getBody().getResult());
 
         //then
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(expectedResponse, responseEntity.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("인증번호를 전송하였습니다.", response.getBody().getResult());
+
     }
 
     @Test
+    @DisplayName(value = "아이디 찾기 - SMS 인증번호 전송 실패 테스트")
     void sendFindIdSmsAuthNumberFailureTest() {
 
         //given
-        String unregisteredPhoneNumber = "01094342762";
-        SmsAuthNumberPostRequest requestDto = new SmsAuthNumberPostRequest(unregisteredPhoneNumber);
+        UserEntity user = new UserEntity();
+        user.setPhoneNumber("01094342762");
+        user.setEmail("test@gmail.com");
+        userRepository.save(user);
 
-        when(loginService.sendFindIdSmsAuthNumber(requestDto)).thenReturn(
-                SendFindSmsAuthNumberPostResponse.builder()
+        SmsAuthNumberPostRequest requestDto = new SmsAuthNumberPostRequest("01012341234");
+
+        when(userLoginService.sendFindIdSmsAuthNumber(requestDto)).thenReturn(
+                FindSmsAuthNumberPostResponse
+                        .builder()
                         .result("입력하신 휴대폰 번호와 일치하는 정보가 없습니다. \n(고객센터 문의 요망)")
                         .build()
         );
 
         //when
-        ResponseEntity<SendFindSmsAuthNumberPostResponse> response = loginController.sendFindIdSmsAuthNumber(requestDto);
-        System.out.println("response = " + response.getBody().getResult());
+        ResponseEntity<FindSmsAuthNumberPostResponse> response = userLoginController.sendFindIdSmsAuthNumber(requestDto);
+        System.out.println("sendFindIdSmsAuthNumberFailureTest response.getBody().getResult() = " + response.getBody().getResult());
 
         //then
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -71,52 +108,219 @@ class UserLoginControllerTest {
     }
 
     @Test
-    void verifySmsAuthAndFindIdSuccessTest() {
+    @DisplayName(value = "아이디 찾기 - SMS 인증번호 확인 및 아이디 찾기 성공 테스트")
+    void checkFindIdSmsAuthSuccessTest() {
 
         // given
-        FindIdVerifySmsRequest requestDto = new FindIdVerifySmsRequest("01012345678", "1234");
-        when(loginService.verifySmsAuth("01012345678", "1234")).thenReturn(true);
-        when(loginService.getEmailByPhoneNumber("01012345678")).thenReturn("example@example.com");
+        UserEntity user = new UserEntity();
+        user.setPhoneNumber("01094342762");
+        user.setEmail("test@gmail.com");
+        userRepository.save(user);
+
+        SmsAuthEntity smsAuth = new SmsAuthEntity();
+        smsAuth.setSmsAuthNumber("1234");
+        smsAuth.setExpirationTime(LocalDateTime.now().plusMinutes(30));
+        smsAuthRepository.save(smsAuth);
+
+        FindIdCheckSmsGetRequest requestDto = new FindIdCheckSmsGetRequest(1L, user.getPhoneNumber(), "1234");
+
+        when(userLoginService.checkFindIdSmsAuth(requestDto)).thenReturn(
+                FindSmsAuthNumberGetResponse
+                        .builder()
+                        .result("회원님의 아이디는 'test@gmail.com' 입니다.")
+                        .build());
+
+        when(userRepository.findByPhoneNumber("01094342762")).thenReturn(Optional.of(user));
+        when(smsAuthRepository.findById(1L)).thenReturn(Optional.of(smsAuth));
 
         // when
-        ResponseEntity<String> response = loginController.verifySmsAuthAndFindId(requestDto);
-        System.out.println("testVerifySmsAuthAndFindId_Success : " + response);
+        ResponseEntity<FindSmsAuthNumberGetResponse> response = userLoginController.checkFindIdSmsAuth(requestDto);
+        System.out.println("checkFindIdSmsAuthSuccessTest response.getBody().getResult() = " + response.getBody().getResult());
 
         // then
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("회원님의 아이디는 'example@example.com' 입니다.", response.getBody());
+        assertEquals("회원님의 아이디는 'test@gmail.com' 입니다.", response.getBody().getResult());
     }
+
     @Test
-    void verifySmsAuthAndFindIdFailureTest() {
+    @DisplayName(value = "아이디 찾기 - SMS 인증번호 확인 및 아이디 찾기 실패 테스트")
+    void checkFindIdSmsAuthFailureTest() {
 
         // given
-        FindIdVerifySmsRequest requestDto = new FindIdVerifySmsRequest("01012345678", "1111");
-        when(loginService.verifySmsAuth("01012345678", "1111")).thenReturn(false);
+        FindIdCheckSmsGetRequest requestDto = new FindIdCheckSmsGetRequest(1L, "01094342762", "1234");
+
+        SmsAuthEntity smsAuth = new SmsAuthEntity();
+        smsAuth.setSmsAuthNumber("5678");
+        smsAuth.setExpirationTime(LocalDateTime.now().minusMinutes(30));
+        smsAuthRepository.save(smsAuth);
+
+        when(userLoginService.checkFindIdSmsAuth(requestDto)).thenReturn(
+                FindSmsAuthNumberGetResponse
+                        .builder()
+                        .result("입력하신 휴대폰 번호와 일치하는 아이디 정보가 없습니다. \n(고객센터 문의 요망)")
+                        .build());
+        when(smsAuthRepository.findById(1L)).thenReturn(Optional.of(smsAuth));
 
         // when
-        ResponseEntity<String> response = loginController.verifySmsAuthAndFindId(requestDto);
-        System.out.println("testVerifySmsAuthAndFindId_Failure: " + response);
+        ResponseEntity<FindSmsAuthNumberGetResponse> response = userLoginController.checkFindIdSmsAuth(requestDto);
+        System.out.println("checkFindIdSmsAuthFailureTest response.getBody().getResult() = " + response.getBody().getResult());
 
         // then
-        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-        assertEquals("SMS 인증에 실패하였습니다. \n인증번호를 정상적으로 입력해주세요.", response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("입력하신 휴대폰 번호와 일치하는 아이디 정보가 없습니다. \n(고객센터 문의 요망)", response.getBody().getResult());
+    }
+
+    // ************* 비밀번호 찾기 *************
+
+    @Test
+    @DisplayName(value = "비밀번호 찾기 - SMS 인증번호 전송 성공 테스트")
+    void sendFindPwSmsAuthNumberSuccessTest(){
+
+        //given
+        UserEntity user = new UserEntity();
+        user.setPhoneNumber("01094342762");
+        user.setEmail("test@gmail.com");
+        userRepository.save(user);
+
+        PassResetSmsAuthNumberPostRequest requestDto = new PassResetSmsAuthNumberPostRequest(user.getPhoneNumber(), user.getEmail());
+        System.out.println("requestDto.getPhoneNumber() = " + requestDto.getPhoneNumber());
+        System.out.println("requestDto.getEmail() = " + requestDto.getEmail());
+
+        when(userLoginService.sendFindPwSmsAuthNumber(requestDto)).thenReturn(
+                FindSmsAuthNumberPostResponse
+                        .builder()
+                        .result("인증번호를 전송하였습니다.")
+                        .build()
+        );
+
+        //when
+        ResponseEntity<FindSmsAuthNumberPostResponse> response = userLoginController.sendFindPwSmsAuthNumber(requestDto);
+        System.out.println("sendFindPwSmsAuthNumberSuccessTest response.getBody().getResult() = " + response.getBody().getResult());
+
+        //then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("인증번호를 전송하였습니다.", response.getBody().getResult());
+
+
     }
 
     @Test
-    void verifySmsAuthAndFindIdNotFoundTest() {
+    @DisplayName(value = "비밀번호 찾기 - SMS 인증번호 전송 실패 테스트")
+    void sendFindPwSmsAuthNumberFailureTest(){
 
-        // given
-        FindIdVerifySmsRequest requestDto = new FindIdVerifySmsRequest("01012345678", "0000");
-        when(loginService.verifySmsAuth("01012345678", "0000")).thenReturn(true);
-        when(loginService.getEmailByPhoneNumber("01012345678")).thenReturn(null);
+        //given
+        UserEntity user = new UserEntity();
+        user.setPhoneNumber("01094342762");
+        user.setEmail("test@gmail.com");
+        userRepository.save(user);
+
+        PassResetSmsAuthNumberPostRequest requestDto = new PassResetSmsAuthNumberPostRequest("01012341234", "dkdkdk@gmail.com");
+        System.out.println("requestDto.getPhoneNumber() = " + requestDto.getPhoneNumber());
+        System.out.println("requestDto.getEmail() = " + requestDto.getEmail());
+
+        when(userLoginService.sendFindPwSmsAuthNumber(requestDto)).thenReturn(
+                FindSmsAuthNumberPostResponse
+                        .builder()
+                        .result("입력하신 휴대폰 번호와 일치하는 정보가 없습니다. \n(고객센터 문의 요망)")
+                        .build()
+        );
 
         //when
-        ResponseEntity<String> response = loginController.verifySmsAuthAndFindId(requestDto);
-        System.out.println("testVerifySmsAuthAndFindId_NotFound: " + response);
+        ResponseEntity<FindSmsAuthNumberPostResponse> response = userLoginController.sendFindPwSmsAuthNumber(requestDto);
+        System.out.println("sendFindPwSmsAuthNumberSuccessTest response.getBody().getResult() = " + response.getBody().getResult());
+
+        //then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("입력하신 휴대폰 번호와 일치하는 정보가 없습니다. \n(고객센터 문의 요망)", response.getBody().getResult());
+
+    }
+
+    @Test
+    @DisplayName(value = "비밀번호 찾기 - SMS 인증번호 확인 및 비밀번호 초기화 성공 테스트")
+    void checkFindPwSmsAuthAndResetSuccessTest() {
+
+        // given
+        UserEntity user = new UserEntity();
+        user.setPhoneNumber("01094342762");
+        user.setEmail("test@gmail.com");
+        userRepository.save(user);
+
+        SmsAuthEntity smsAuth = new SmsAuthEntity();
+        smsAuth.setSmsAuthNumber("1234");
+        smsAuth.setExpirationTime(LocalDateTime.now().plusMinutes(30));
+        smsAuthRepository.save(smsAuth);
+
+        FindPwCheckSmsGetRequest requestDto = new FindPwCheckSmsGetRequest(1L, user.getPhoneNumber(), "1234", user.getEmail());
+
+        String newPassword = tempPassword(10);
+        System.out.println("newPassword = " + newPassword);
+
+        when(userLoginService.checkFindPwSmsAuth(requestDto)).thenReturn(
+                FindSmsAuthNumberGetResponse
+                        .builder()
+                        .result("비밀번호가 '" + newPassword + "'로 초기화 되었습니다.")
+                        .build());
+
+        when(userRepository.findByPhoneNumber("01094342762")).thenReturn(Optional.of(user));
+        when(userRepository.findByEmail("test@gmail.com")).thenReturn(Optional.of(user));
+        when(smsAuthRepository.findById(1L)).thenReturn(Optional.of(smsAuth));
+
+        // when
+        ResponseEntity<FindSmsAuthNumberGetResponse> response = userLoginController.checkFindPwSmsAuthAndReset(requestDto);
+        System.out.println("checkFindPwSmsAuthAndResetSuccessTest response.getBody().getResult() = " + response.getBody().getResult());
 
         // then
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-        assertEquals("입력하신 휴대폰 번호와 일치하는 정보가 없습니다. \n(고객센터 문의 요망)", response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("비밀번호가 '" + newPassword + "'로 초기화 되었습니다.", response.getBody().getResult());
+    }
+
+    @Test
+    @DisplayName(value = "비밀번호 찾기 - SMS 인증번호 확인 및 비밀번호 초기화 실패 테스트")
+    void checkFindPwSmsAuthAndResetFailureTest() {
+
+        // given
+        UserEntity user = new UserEntity();
+        user.setPhoneNumber("01094342762");
+        user.setEmail("test@gmail.com");
+        userRepository.save(user);
+
+        SmsAuthEntity smsAuth = new SmsAuthEntity();
+        smsAuth.setSmsAuthNumber("5678");
+        smsAuth.setExpirationTime(LocalDateTime.now().minusMinutes(30));
+        smsAuthRepository.save(smsAuth);
+
+        FindPwCheckSmsGetRequest requestDto = new FindPwCheckSmsGetRequest(1L, "01012543658", "1234", user.getEmail());
+
+        when(userLoginService.checkFindPwSmsAuth(requestDto)).thenReturn(
+                FindSmsAuthNumberGetResponse
+                        .builder()
+                        .result("입력하신 휴대폰 번호와 이메일 정보가 일치하는 사용자가 없습니다.")
+                        .build());
+
+        when(userRepository.findByPhoneNumber("01012543658")).thenReturn(Optional.of(user));
+        when(userRepository.findByEmail("test@gmail.com")).thenReturn(Optional.of(user));
+        when(smsAuthRepository.findById(1L)).thenReturn(Optional.of(smsAuth));
+
+        // when
+        ResponseEntity<FindSmsAuthNumberGetResponse> response = userLoginController.checkFindPwSmsAuthAndReset(requestDto);
+        System.out.println("checkFindPwSmsAuthAndResetFailureTest response.getBody().getResult() = " + response.getBody().getResult());
+
+        // then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("입력하신 휴대폰 번호와 이메일 정보가 일치하는 사용자가 없습니다.", response.getBody().getResult());
+    }
+
+    //임시 비밀번호 생성
+    public String tempPassword(int len) {
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < len; i++) {
+            int index = secureRandom.nextInt(CHAR_SET.length());
+            char randomChar = CHAR_SET.charAt(index);
+            password.append(randomChar);
+        }
+
+        return password.toString();
     }
 
 }
