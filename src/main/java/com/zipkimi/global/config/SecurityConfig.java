c36dc3a53e1eb1a7fc87d617b7091cb0c51f76c2
Woolly <wool_ly@naver.com>
2023-08-22 08:57:35 +0900

    Chore : SpringSecurity 설정 파일 수정 (RestAPI)
    - RestAPI에 필요한 SpringSecurity 설정 파일 수정
    
    Related to: #27

--- src/main/java/com/zipkimi/global/config/SecurityConfig.java
+++ src/main/java/com/zipkimi/global/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.zipkimi.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -12,14 +14,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic().disable() // REST API 이므로 기본설정 미사용
+                .csrf().disable() // REST API 이므로 csrf 보안 미사용
+                .formLogin().disable() // REST API 이므로 formLogin 미사용
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT를 통해 인증하므로 세션 미사용
+                .and()
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .defaultSuccessUrl("/")
-                    .permitAll()
-                .and()
-                .logout();
-
+                .antMatchers("/api/v1/userMgmt/**").permitAll()
+                .antMatchers("/api/v1/users/**").permitAll()
+                .antMatchers("/exception/**").permitAll()
+                .anyRequest().authenticated();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 예외 처리
+        //web.ignoring().antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**");
+    }
+
+
 }
