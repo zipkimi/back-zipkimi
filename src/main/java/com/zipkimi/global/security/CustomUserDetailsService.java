df3a347d7ac7c9b8d8e78e54058dd95197a2e2a4
Woolly <wool_ly@naver.com>
2023-08-25 12:41:54 +0900

    Refactor : SpringSecurity + JWT 관련 코드 정리
    - 불필요 코드 정리
    
    Related to: #27

--- src/main/java/com/zipkimi/global/security/CustomUserDetailsService.java
+++ src/main/java/com/zipkimi/global/security/CustomUserDetailsService.java
@@ -25,25 +25,29 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         log.info(" ===== CustomUserDetailsService loadUserByUsername ===== ");
-        log.info("CustomUserDetailsService email = " + email);
+        log.info(" ===== CustomUserDetailsService email = " + email);
         return userRepository.findByEmail(email)
                 .map(this::createUserDetails)
                 .orElseThrow(
-                        () -> new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다."));
+                        () -> new UsernameNotFoundException(email + " -> 해당하는 유저를 찾을 수 없습니다."));
     }
 
     // DB 에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
     private UserDetails createUserDetails(UserEntity user) {
+
+        // GrantedAuthority로 user의 권한을 가져 옴.
         GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getRole().toString());
         log.info(" ===== CustomUserDetailsService createUserDetails ===== ");
         log.info("CustomUserDetailsService grantedAuthority = " + grantedAuthority);
-        log.info("CustomUserDetailsService user = " + user);
+        log.info("CustomUserDetailsService user.getUserId = " + user.getUserId());
+        log.info("CustomUserDetailsService user.getPassword = " + user.getPassword());
 
         return new User(
                 String.valueOf(user.getUserId()),
                 user.getPassword(),
                 Collections.singleton(grantedAuthority)
         );
+
     }
 
 
