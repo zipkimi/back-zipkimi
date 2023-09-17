24f07ceb52522171ea5ec8ca1a57c65214c9d98d
Woolly <wool_ly@naver.com>
2023-09-17 22:39:42 +0900

    Feat : BuilderManagementController checkBuilderUserJoinSmsAuthNumber 메서드 추가 - BuilderManagementController checkBuilderUserJoinSmsAuthNumber 메서드 추가
    Related to: #40

--- src/main/java/com/zipkimi/builder/controller/BuilderManagementController.java
+++ src/main/java/com/zipkimi/builder/controller/BuilderManagementController.java
@@ -1,7 +1,9 @@
 package com.zipkimi.builder.controller;
 
 import com.zipkimi.builder.service.BuilderManagementService;
+import com.zipkimi.user.dto.request.SmsAuthNumberGetRequest;
 import com.zipkimi.user.dto.request.SmsAuthNumberPostRequest;
+import com.zipkimi.user.dto.response.SmsAuthNumberGetResponse;
 import com.zipkimi.user.dto.response.SmsAuthNumberPostResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,6 +11,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,4 +34,14 @@ public class BuilderManagementController {
         return ResponseEntity.status(HttpStatus.OK).body(builderManagementService.sendBuilderUserJoinSmsAuthNumber(requestDto));
     }
 
+    @ApiOperation(value = "SMS 인증번호 확인")
+    @GetMapping(value = "/sms")
+    public ResponseEntity<SmsAuthNumberGetResponse> checkBuilderUserJoinSmsAuthNumber(
+            @ModelAttribute
+            SmsAuthNumberGetRequest requestDto) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(builderManagementService.checkBuilderUserJoinSmsAuthNumber(requestDto));
+
+    }
 }
