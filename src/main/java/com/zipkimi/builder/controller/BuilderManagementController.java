a0b3764af4cdf8275f8bdab476db64714c0b5cbf
Woolly <wool_ly@naver.com>
2023-09-17 14:19:41 +0900

    Feat : BuilderManagementController 시공사 회원가입 - SMS 인증번호 전송 메서드 추가 - BuilderManagementService 시공사 회원가입 - SMS 인증번호 전송 메서드 추가
    Related to: #40

--- src/main/java/com/zipkimi/builder/controller/BuilderManagementController.java
+++ src/main/java/com/zipkimi/builder/controller/BuilderManagementController.java
@@ -1,9 +1,16 @@
 package com.zipkimi.builder.controller;
 
 import com.zipkimi.builder.service.BuilderManagementService;
+import com.zipkimi.user.dto.request.SmsAuthNumberPostRequest;
+import com.zipkimi.user.dto.response.SmsAuthNumberPostResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @Api(value = "시공사 회원 관리")
-@RequestMapping("/api/v1/builders")
+@RequestMapping("/api/v1/builderMgmt/builders")
 public class BuilderManagementController {
 
     private BuilderManagementService builderManagementService;
+
+    @ApiOperation(value = "SMS 인증번호 전송")
+    @PostMapping(value = "/sms")
+    public ResponseEntity<SmsAuthNumberPostResponse> sendBuilderUserJoinSmsAuthNumber(@RequestBody
+            SmsAuthNumberPostRequest requestDto){
+        return ResponseEntity.status(HttpStatus.OK).body(builderManagementService.sendBuilderUserJoinSmsAuthNumber(requestDto));
+    }
 
 }
