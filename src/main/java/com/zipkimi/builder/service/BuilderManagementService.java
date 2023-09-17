2e5d91f2b0005ed24d5cf01a66fe28a8610e3993
Woolly <wool_ly@naver.com>
2023-09-17 22:39:16 +0900

    Feat : BuilderManagementService checkBuilderUserJoinSmsAuthNumber 메서드 추가 - BuilderManagementService checkBuilderUserJoinSmsAuthNumber 메서드 추가 - sendBuilderUserJoinSmsAuthNumber 메서드에 setIsUse(false) 추가
    Related to: #40

--- src/main/java/com/zipkimi/builder/service/BuilderManagementService.java
+++ src/main/java/com/zipkimi/builder/service/BuilderManagementService.java
@@ -13,7 +13,9 @@ import com.zipkimi.repository.BuilderRepository;
 import com.zipkimi.repository.BuilderUserRepository;
 import com.zipkimi.repository.SmsAuthRepository;
 import com.zipkimi.repository.UserRepository;
+import com.zipkimi.user.dto.request.SmsAuthNumberGetRequest;
 import com.zipkimi.user.dto.request.SmsAuthNumberPostRequest;
+import com.zipkimi.user.dto.response.SmsAuthNumberGetResponse;
 import com.zipkimi.user.dto.response.SmsAuthNumberPostResponse;
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -42,20 +44,23 @@ public class BuilderManagementService {
             SmsAuthNumberPostRequest requestDto) {
 
         // 휴대폰 번호 유효성 검사 - 타입, 글자수, 이미 등록된 번호인지 체크
-        String phoneNumber = requestDto.getPhoneNumber().replaceAll("\\D", ""); // "\\D" 정규식을 사용하여 숫자이외의 문자는 모두 ""으로 변경
+        String phoneNumber = requestDto.getPhoneNumber()
+                .replaceAll("\\D", ""); // "\\D" 정규식을 사용하여 숫자이외의 문자는 모두 ""으로 변경
         if (phoneNumber == null || !isValidPhoneNumber(phoneNumber)) {
             return SmsAuthNumberPostResponse.builder()
                     .message("휴대폰 번호를 정확히 입력해주세요.")
                     .build();
         }
 
-        // 시공사 회원 중복 확인
-        Optional<BuilderUserEntity> builderUser = builderUserRepository.findByPhoneNumberAndIsUseIsTrue(phoneNumber);
-        // 일반 회원 중복 확인
-        Optional<UserEntity> user = userRepository.findByPhoneNumberAndIsUseIsTrue(phoneNumber);
+        // 휴대폰번호 & 사용여부로 일반 회원 정보 조회 - 일반 회원 중복 확인
+        Optional<UserEntity> optionalUser = userRepository.findByPhoneNumberAndIsUseIsTrue(phoneNumber);
+
+        // 휴대폰번호 & 사용여부로 시공사 회원 정보 조회 - 시공사 회원 중복 확인
+        Optional<BuilderUserEntity> optionalBuilderUser = builderUserRepository.findByPhoneNumberAndIsUseIsTrue(
+                phoneNumber);
 
         // 일반 회원으로 가입된 휴대폰 번호거나, 시공사 회원으로 가입된 휴대폰 번호일 경우
-        if (builderUser.isPresent() || user.isPresent()) {
+        if (optionalBuilderUser.isPresent() || optionalUser.isPresent()) {
             return SmsAuthNumberPostResponse.builder()
                     .message("이미 등록된 휴대폰 번호입니다.")
                     .build();
@@ -68,7 +73,6 @@ public class BuilderManagementService {
 
         String randomNumber;
         SmsAuthEntity smsAuthEntitySaved = null;
-        log.info("randomNumber = {}, randomNumber");
 
         if (existingSmsAuth != null) {
             // SMS 인증번호가 만료되지 않았을 경우
@@ -84,12 +88,14 @@ public class BuilderManagementService {
             SmsAuthEntity smsAuth = new SmsAuthEntity();
             smsAuth.setPhoneNumber(phoneNumber);
             smsAuth.setSmsAuthNumber(randomNumber);
+            smsAuth.setIsUse(false);
             smsAuth.setIsAuthenticate(false);
             smsAuth.setExpirationTime(LocalDateTime.now().plusMinutes(5L));
             smsAuth.setSmsAuthType(SMS_AUTH_CODE.BUILDER_JOIN.getValue());
 
             // SMS 내용 설정
-            smsAuth.setContent("[집킴이] 본인확인 인증번호는 [" + smsAuth.getSmsAuthNumber() + "] 입니다. 인증번호를 정확히 입력해주세요.");
+            smsAuth.setContent(
+                    "[집킴이] 본인확인 인증번호는 [" + smsAuth.getSmsAuthNumber() + "] 입니다. 인증번호를 정확히 입력해주세요.");
 
             try {
                 // DB 테이블에 insert
@@ -97,6 +103,7 @@ public class BuilderManagementService {
 
                 // SMS 전송 로직
                 smsService.pushSMSMessage(smsAuthEntitySaved);
+                System.out.println("!!!!!!!!!! smsAuthEntitySaved1 = " + smsAuthEntitySaved);
 
             } catch (Exception e) {
                 throw new BadRequestException("SMS 인증번호를 생성하던 중 오류가 발생하였습니다.");
@@ -108,5 +115,51 @@ public class BuilderManagementService {
                 .message("인증번호를 전송하였습니다.")
                 .build();
     }
+
+    public SmsAuthNumberGetResponse checkBuilderUserJoinSmsAuthNumber(
+            SmsAuthNumberGetRequest requestDto) {
+
+        // #1. SMS 인증번호를 ID를 통해 DB 데이터 검증 후
+        Optional<SmsAuthEntity> smsAuth = smsAuthRepository.findById(requestDto.getSmsAuthId());
+        log.info("smsAuth ={}", smsAuth);
+        log.info("smsAuth.get().getSmsAuthNumber() ={}", smsAuth.get().getSmsAuthNumber());
+
+        if (smsAuth.isEmpty() || !smsAuth.get().getSmsAuthNumber()
+                .equals(requestDto.getSmsAuthNumber())) {
+            return SmsAuthNumberGetResponse
+                    .builder()
+                    .message("SMS 인증에 실패하였습니다. \n인증번호를 정상적으로 입력해주세요.")
+                    .build();
+        }
+
+        // 2. SMS 인증번호 만료시간 검증
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDateTime expirationTime = smsAuth.get().getExpirationTime();
+
+        if (currentTime.isAfter(expirationTime)) {
+            return SmsAuthNumberGetResponse
+                    .builder()
+                    .message("인증번호가 만료되었습니다. \n다시 인증번호를 발급받아주세요.")
+                    .build();
+        }
+
+        if (Boolean.TRUE.equals(smsAuth.get().getIsUse())) {
+            return SmsAuthNumberGetResponse
+                    .builder()
+                    .message("이미 사용된 인증번호입니다. \n다시 인증번호를 발급받아주세요.")
+                    .build();
+        }
+
+        // SMS 인증번호를 여러 번 사용하는 것 방지
+        smsAuth.get().setIsUse(true);
+        // SMS 본인 인증 완료 여부를 true로 업데이트
+        smsAuth.get().setIsAuthenticate(true);
+        smsAuthRepository.save(smsAuth.get());
+
+        return SmsAuthNumberGetResponse.builder()
+                .message("본인 인증에 성공했습니다.")
+                .build();
+    }
+
 
 }
