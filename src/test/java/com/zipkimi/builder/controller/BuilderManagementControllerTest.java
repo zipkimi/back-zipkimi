95bfe2a3dfa9065e2ff03a96b47991ab0f369fb6
Woolly <wool_ly@naver.com>
2023-10-08 22:26:58 +0900

    Test : 시공사 회원가입 Controller 테스트 클래스 생성
    - 시공사 회원가입 Controller 테스트 클래스 생성
    - BuilderManagementControllerTest 생성
    
    Related to: #55

---
+++ src/test/java/com/zipkimi/builder/controller/BuilderManagementControllerTest.java
@@ -0,0 +1,12 @@
+package com.zipkimi.builder.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+class BuilderManagementControllerTest {
+
+}
