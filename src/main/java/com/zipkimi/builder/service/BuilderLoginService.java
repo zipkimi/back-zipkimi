01747a47a7d64d1e4e76692ae72e88dbd1f24353
promiseyoo <ypr821@gmail.com>
2023-11-15 23:20:17 +0900

    feat: #42 시공사 로그인 기능 추가-1

--- src/main/java/com/zipkimi/builder/service/BuilderLoginService.java
+++ src/main/java/com/zipkimi/builder/service/BuilderLoginService.java
@@ -2,7 +2,9 @@ package com.zipkimi.builder.service;
 
 import com.zipkimi.builder.dto.request.BuilderLoginRequest;
 import com.zipkimi.global.jwt.dto.response.TokenResponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BuilderLoginService {
 
     public TokenResponse login(BuilderLoginRequest request) {
